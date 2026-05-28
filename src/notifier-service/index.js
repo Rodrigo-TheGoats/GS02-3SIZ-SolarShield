b8369c629fd1b419ef876c01d126c79044f9fc59
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-28 19:54:02 -0300

    feat(notifier-service): Implementação para iniciar o serviço de notificação

---
+++ src/notifier-service/index.js
@@ -0,0 +1,25 @@
+const amqp = require("amqplib");
+const Redis = require("ioredis");
+const { logger } = require("./logging");
+const { NotifierService } = require("./notifier");
+require("dotenv").config();
+
+const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const RABBITMQ_URL = process.env.RABBITMQ_URL || "amqp://localhost";
+
+const redis = new Redis(REDIS_URL);
+
+async function start() {
+  const connection = await amqp.connect(RABBITMQ_URL);
+  const channel = await connection.createChannel();
+
+  const notifierService = new NotifierService({
+    redis,
+    channel,
+    logger: logger,
+  });
+
+  await notifierService.setupRabbitMQ();
+}
+
+start();
